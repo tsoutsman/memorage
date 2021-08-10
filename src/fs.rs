e9bfb09770ec2a3422bf47634c4803d6519519b2
Klim Tsoutsman <klimusha@gmail.com>
2021-08-11 00:34:25 +1000

    Refactor tests

--- src/fs.rs
+++ src/fs.rs
@@ -39,31 +39,61 @@ mod tests {
     use super::*;
     use tempfile::tempdir;
 
+    macro_rules! gen_fs {
+        ($root:ident => ) => {};
+        ($root:ident => $f:ident $($tail:tt)*) => {
+            ::std::fs::File::create($root.join(stringify!($f))).unwrap();
+            gen_fs!($root => $($tail)*);
+        };
+        ($root:ident => ($dir:ident : $($inner:tt)*)$($tail:tt)*) => {
+            ::std::fs::create_dir($root.join(stringify!($dir))).unwrap();
+            let $dir = $root.join(stringify!($dir));
+            gen_fs!($dir => $($inner)*);
+            gen_fs!($root => $($tail)*);
+        };
+    }
+
+    macro_rules! gen_expected {
+        // The first 2 patterns are only called the first time by the user. The tt is only munched
+        // by the last 3 patterns.
+        ($root:ident => $f:ident $($tail:tt)*) => {
+            {
+                #[allow(clippy::vec_init_then_push)]
+                {
+                    let mut z_macro: Vec<PathBuf> = Vec::new();
+                    gen_expected!(@$root | z_macro => $f $($tail)*);
+                    z_macro
+                }
+            }
+        };
+        ($root:ident => ($dir:ident : $($inner:tt)*)$($tail:tt)*) => {
+            {
+                #[allow(clippy::vec_init_then_push)]
+                {
+                    let mut z_macro: Vec<PathBuf> = Vec::new();
+                    gen_expected!(@$root | z_macro => ($dir : $($inner)*) $($tail)*);
+                    z_macro
+                }
+            }
+        };
+        (@$root:ident | $vec:ident => ) => {};
+        (@$root:ident | $vec:ident => $f:ident $($tail:tt)*) => {
+            $vec.push($root.join(stringify!($f)));
+            gen_expected!(@$root | $vec => $($tail)*);
+        };
+        (@$root:ident | $vec:ident => ($dir:ident : $($inner:tt)*)$($tail:tt)*) => {
+            let $dir = $root.join(stringify!($dir));
+            gen_expected!(@$dir | $vec => $($inner)*);
+            gen_expected!(@$root | $vec => $($tail)*);
+        };
+    }
+
     #[test]
     fn test_changed_files() {
-        // TODO
-        // The setup in this test is very janky. It will definetly be fixed.
         let root_path = tempdir().unwrap().into_path();
-
-        let files = vec!["one", "two"];
-        let directories = vec![("temp-directory", vec!["foo", "bar"])];
-
-        for file in files {
-            let file_path = root_path.join(file);
-            fs::File::create(file_path).unwrap();
-        }
-        for (dir, children) in directories {
-            let dir_path = root_path.join(dir);
-            fs::create_dir(dir_path.clone()).unwrap();
-
-            for file in children {
-                let file_path = dir_path.join(file);
-                fs::File::create(file_path).unwrap();
-            }
-        }
+        gen_fs!(root_path => bar (x: temp foo));
 
         let expected: Vec<PathBuf> = Vec::new();
-        // Actual testing
         assert_eq!(
             expected,
             changed_files(root_path.clone(), Duration::from_secs(1)).unwrap()
@@ -71,16 +101,12 @@ mod tests {
 
         std::thread::sleep(Duration::from_secs(1));
 
-        let mut expected: Vec<PathBuf> = vec![
-            root_path.join("one"),
-            root_path.join("two"),
-            root_path.join("temp-directory").join("foo"),
-            root_path.join("temp-directory").join("bar"),
-        ];
+        let mut expected = gen_expected!(root_path => bar (x: temp foo));
         expected.sort();
 
         let mut result = changed_files(root_path, Duration::from_secs(1)).unwrap();
         result.sort();
+
         assert_eq!(expected, result);
     }
 }
