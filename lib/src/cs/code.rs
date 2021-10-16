f979cd926b20406ef5c86bdc6a19d97b00a51564
Klim Tsoutsman <klimusha@gmail.com>
2021-10-16 21:24:37 +1100

    Document `Code`
    Signed-off-by: Klim Tsoutsman <klimusha@gmail.com>

--- lib/src/cs/code.rs
+++ lib/src/cs/code.rs
@@ -2,10 +2,14 @@ use rand::{distributions::Alphanumeric, Rng};
 use rand_chacha::{rand_core::SeedableRng, ChaCha20Rng};
 use serde::{Deserialize, Serialize};
 
+/// Struct representing the code used during pairing.
+///
+/// The code consists of [`LEN`](Self::LEN) alphanumeric characters.
 #[derive(Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub struct Code(String);
 
 impl Code {
+    /// The length of the code in characters.
     pub const LEN: usize = 6;
 
     pub fn new() -> Self {
@@ -29,6 +33,7 @@ impl Default for Code {
 impl std::convert::TryFrom<String> for Code {
     type Error = crate::cs::error::Error;
 
+    /// Returns an error if the [`String`] is of incorrect length.
     fn try_from(c: String) -> Result<Self, Self::Error> {
         if c.len() == Self::LEN {
             Ok(Self(c))
