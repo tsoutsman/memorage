d5a17b90b598ccb461a34015f72a284b43a4dde7
Klim Tsoutsman <klimusha@gmail.com>
2022-06-10 21:56:06 +1000

    Add GUI boilerplate
    Signed-off-by: Klim Tsoutsman <klimusha@gmail.com>

--- crates/client-gui/src/main.rs
+++ crates/client-gui/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use iced::{Command, Element, Text};
+
+struct App;
+
+impl iced::Application for App {
+    type Executor = iced::executor::Default;
+    type Message = ();
+    type Flags = ();
+
+    fn new(_flags: ()) -> (Self, Command<Self::Message>) {
+        (Self, Command::none())
+    }
+
+    fn title(&self) -> String {
+        String::from("Memorage")
+    }
+
+    fn update(&mut self, _message: Self::Message) -> Command<Self::Message> {
+        Command::none()
+    }
+
+    fn view(&mut self) -> Element<Self::Message> {
+        Text::new("Hello, world!").into()
+    }
+}
+
+fn main() -> iced::Result {
+    <App as iced::Application>::run(iced::Settings::default())
 }
